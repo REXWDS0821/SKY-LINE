6e97cb2db84884e07efb1ed5b34861e0a8f99d66
Rex <1780692450@qq.com>
2020-10-23 20:32:11 -0400

    Assignment 1

---
+++ Assets/Scripts/MoveOnToch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnToch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.collider.transform.SetParent(transform);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.collider.transform.SetParent(transform);
+        }
+    }
+
+}
---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    private Transform playerDefTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerDefTransform = GameObject.FindGameObjectWithTag("Player").transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player")&& other.GetType().ToString() == "UnityEngine.BoxCollider2D")
+        {
+            other.gameObject.transform.parent = gameObject.transform;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && other.GetType().ToString() == "UnityEngine.BoxCollider2D")
+        {
+            other.gameObject.transform.parent = playerDefTransform;
+        }
+    }
+}
